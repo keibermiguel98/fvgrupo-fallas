cb1d4c22c123bb7396aaf427c294f7ea943c6c8b
Keiber linarez <keibermiguel98@gmail.com>
2024-04-27 17:09:55 -0400

    origin main

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage} from 'firebase/storage'
+//import { getStorage} from 'firebase/storage'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,5 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
 export const database = getFirestore(app)
